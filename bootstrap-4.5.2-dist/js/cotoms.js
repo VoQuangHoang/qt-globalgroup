52d5798fc91f004009f981c9976f01416563ead1
phamductrung1133 <trungpdpd02742@fpt.edu.vn>
2020-10-07 22:38:43 +0700

    trung up code

---
+++ bootstrap-4.5.2-dist/js/cotoms.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.carouselSlick').slick({
+      dots: true,
+  infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      centerMode: true,
+      variableWidth: true,
+      centerPadding:'0',
+      arrows: false,
+    });
+  
+        
+  });
